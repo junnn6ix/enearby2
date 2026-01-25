f885b3d955f1d6a4b3ce49e6853bf07dfefd2bfa
junnn6ix <ngntdluxd@gmail.com>
2026-01-25 21:33:32 +0700

    feat: populate NavbarSidebar with navigation links, login/signup actions, and a copyright footer.

--- src/components/NavbarSidebar.tsx
+++ src/components/NavbarSidebar.tsx
@@ -1,4 +1,13 @@
-import { Sheet, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
+import Link from "next/link";
+import { ScrollArea } from "./ui/scroll-area";
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+} from "./ui/sheet";
+import { ChevronRight, Copyright } from "lucide-react";
 
 interface NavbarItem {
   href: string;
@@ -14,12 +23,39 @@ interface Props {
 const NavbarSidebar = ({ items, open, onOpenChange }: Props) => {
   return (
     <Sheet open={open} onOpenChange={onOpenChange}>
-      <SheetContent side="right" className="bg-background/70 backdrop-blur-lg">
-        <SheetHeader className="p-4 border-b">
+      <SheetContent
+        side="right"
+        className="bg-background/70 backdrop-blur-lg w-[75vw]">
+        <SheetHeader className="p-4 border-b ">
           <div className="text-2xl font-bold">
             <SheetTitle>eNearby</SheetTitle>
           </div>
         </SheetHeader>
+
+        <ScrollArea className="flex flex-col overflow-y-auto h-full pb-2">
+          {items.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className="w-full text-left p-4 hover:bg-primary hover:text-secondary flex items-center justify-between">
+              {item.children}
+              <ChevronRight />
+            </Link>
+          ))}
+          <div className="text-left border-t border-b rounded-none w-full p-4 bg-transparent text-base hover:bg-pink-400 hover:text-primary transition-colors mt-4">
+            <Link href="/sign-in">Log In</Link>
+          </div>
+          <div className="text-left border-t border-b rounded-none w-full p-4 bg-primary text-secondary text-base hover:bg-pink-400 hover:text-primary transition-colors">
+            <Link href="/sign-up">Start Selling</Link>
+          </div>
+        </ScrollArea>
+        <SheetFooter className="p-0 gap-0">
+          <span className="flex items-center gap-1 text-sm my-12 p-4 text-muted-foreground">
+            <Copyright className="size-3" /> 2026
+            <span className="font-bold text-primary">eNearby</span>
+            ecomms.
+          </span>
+        </SheetFooter>
       </SheetContent>
     </Sheet>
   );
