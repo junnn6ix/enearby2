d355ee09e7405d7b17f430379b9594e8eda742da
junnn6ix <ngntdluxd@gmail.com>
2026-01-27 17:58:43 +0700

    feat: Display subcategory links in the menu and refine the category layout for horizontal scrolling.

--- src/app/(app)/(home)/search-filters/SubcategoryMenu.tsx
+++ src/app/(app)/(home)/search-filters/SubcategoryMenu.tsx
@@ -1,4 +1,5 @@
 import { Category } from "@/payload-types";
+import Link from "next/link";
 
 interface Props {
   category: Category;
@@ -25,8 +26,21 @@ const SubcategoryMenu = ({ category, isOpen, position }: Props) => {
       }}>
       {/* invisible bridge to maintain hover */}
       <div className="h-3 w-60" />
-      <div className="w-60 text-primary rounded-md overflow-hidden border">
-        <p>Subcategories</p>
+      <div
+        style={{
+          backgroundColor,
+        }}
+        className="w-60 text-primary rounded-md overflow-hidden border shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[2px] -translate-y-[2px] dark:shadow-primary">
+        <div>
+          {category.subcategories?.map((subcategory: Category) => (
+            <Link
+              key={subcategory.slug}
+              href="#"
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center underline font-medium dark:text-black dark:hover:text-white">
+              {subcategory.name}
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
