2a8e4908d6d0292b15c161fa0b8bc02d3fc38deb
junnn6ix <ngntdluxd@gmail.com>
2026-01-28 10:32:14 +0700

    feat: implement tRPC for categories and display fetched data on the homepage.

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,3 +1,27 @@
+// import { getQueryClient, trpc } from "@/trpc/server";
+
+// export default async function Home() {
+//   const queryClient = getQueryClient();
+//   const categories = await queryClient.fetchQuery(
+//     trpc.categories.getMany.queryOptions(),
+//   );
+//   return <div>{JSON.stringify(categories)}</div>;
+// }
+
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { useTRPC } from "@/trpc/client";
+
 export default function Home() {
-  return <div>Home</div>;
+  const trpc = useTRPC();
+  const categories = useQuery(trpc.categories.getMany.queryOptions());
+
+  return (
+    <div>
+      <p>is loading: {categories.isLoading}</p>
+      {JSON.stringify(categories.data, null, 2)}
+    </div>
+  );
 }
