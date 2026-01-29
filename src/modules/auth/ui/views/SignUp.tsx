731dd1add2c2e688ac0b66beb6520d6b4b44f478
junnn6ix <ngntdluxd@gmail.com>
2026-01-29 18:16:04 +0700

    feat: Redesign the sign-up page UI with a new layout, branding, and form fields, and update `@hookform/resolvers`.

--- src/modules/auth/ui/views/SignUp.tsx
+++ src/modules/auth/ui/views/SignUp.tsx
@@ -6,6 +6,7 @@ import { useForm } from "react-hook-form";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -15,10 +16,14 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { registerSchema } from "../../types";
 import { z } from "zod";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { poppins } from "@/components/Navbar";
 
 const SignUp = () => {
   const form = useForm<z.infer<typeof registerSchema>>({
     resolver: zodResolver(registerSchema),
+    // mode: "all",
     defaultValues: {
       email: "",
       username: "",
@@ -26,27 +31,46 @@ const SignUp = () => {
     },
   });
 
+  const onSubmit = (values: z.infer<typeof registerSchema>) => {
+    console.log(values);
+  };
+
+  const username = form.watch("username");
+  const usernameErrors = form.formState.errors.username;
+
+  const showPreview = username && !usernameErrors;
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-6">
-      <div className="bg-primary-foreground h-screen w-full lg:col-span-2 overflow-y-auto">
+      <div className="bg-background h-screen w-full lg:col-span-2 overflow-y-auto px-8 lg:p-16">
         {/* TODO: add form */}
-        <div className="flex flex-col gap-4">
-          <Form {...form}>
-            <form>
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Enter your email" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col justify-between gap-4 h-full">
+            {/* Header */}
 
+            <div className="flex items-center justify-between mb-8">
+              <Link href="/">
+                <span className={cn("text-2xl font-bold", poppins.className)}>
+                  eNearby
+                </span>
+              </Link>
+              <Button
+                asChild
+                variant="ghost"
+                className="text-base border-none underline">
+                <Link prefetch href="/sign-in">
+                  Sign In
+                </Link>
+              </Button>
+            </div>
+
+            <h1 className="text-4xl font-medium">
+              Join over 1,598 creators earning money on eNearby
+            </h1>
+
+            <div className="flex flex-col gap-4">
               <FormField
                 control={form.control}
                 name="username"
@@ -54,7 +78,28 @@ const SignUp = () => {
                   <FormItem>
                     <FormLabel>Username</FormLabel>
                     <FormControl>
-                      <Input placeholder="Enter your username" {...field} />
+                      <Input placeholder="johndoe" {...field} />
+                    </FormControl>
+                    <FormDescription
+                      className={cn("hidden", showPreview && "block")}>
+                      Your store will be available at&nbsp;
+                      {/* TODO: use better method to generate preview url */}
+                      <strong>{username}</strong>
+                      .enearby.com
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="johndoe@example.com" {...field} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -68,15 +113,31 @@ const SignUp = () => {
                   <FormItem>
                     <FormLabel>Password</FormLabel>
                     <FormControl>
-                      <Input placeholder="Enter your password" {...field} />
+                      <Input
+                        placeholder="********"
+                        {...field}
+                        type="password"
+                      />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
-            </form>
-          </Form>
-        </div>
+
+              <Button
+                type="submit"
+                variant="elevated"
+                className="mt-6 bg-black text-secondary dark:text-primary hover:bg-pink-400 dark:hover:bg-pink-400 hover:text-black dark:hover:text-black">
+                Create Account
+              </Button>
+            </div>
+
+            <span className="text-xs text-muted-foreground flex items-center justify-center gap-2 underline -mb-6">
+              <Link href="#">License Agreement</Link>
+              <Link href="#">Privacy Policy</Link>
+            </span>
+          </form>
+        </Form>
       </div>
       <div
         className="h-screen w-full lg:col-span-4 hidden lg:block bg-cover bg-center opacity-85"
