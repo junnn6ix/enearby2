6ef0d5086581f172ec12b92d237e543a5df68192
junnn6ix <ngntdluxd@gmail.com>
2026-01-29 23:27:50 +0700

    feat(auth): update sign-in and sign-up pages to use async session checks and redirect users if authenticated feat(Navbar): conditionally render navigation buttons based on user session state refactor(trpc): enhance server setup with stable query client and caller export

--- src/app/(app)/(auth)/sign-in/page.tsx
+++ src/app/(app)/(auth)/sign-in/page.tsx
@@ -1,6 +1,14 @@
+import { redirect } from "next/navigation";
+import { caller } from "@/trpc/server";
 import SignIn from "@/modules/auth/ui/views/SignIn";
 
-const Page = () => {
+const Page = async () => {
+  const session = await caller.auth.session();
+
+  if (session.user) {
+    redirect("/");
+  }
+
   return <SignIn />;
 };
 
--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -6,9 +6,13 @@ import { makeQueryClient } from "./query-client";
 import { appRouter } from "./routers/_app";
 // IMPORTANT: Create a stable getter for the query client that
 //            will return the same client during the same request.
+
 export const getQueryClient = cache(makeQueryClient);
+
 export const trpc = createTRPCOptionsProxy({
   ctx: createTRPCContext,
   router: appRouter,
   queryClient: getQueryClient,
 });
+
+export const caller = appRouter.createCaller(createTRPCContext);
